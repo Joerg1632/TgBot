987e70765da64c8647c6964113e9df7072e6aa5d
Joerg1632 <131969860+Joerg1632@users.noreply.github.com>
2025-10-01 11:24:03 +0700

    Update wb_api.py

--- wb_api.py
+++ wb_api.py
@@ -34,22 +34,20 @@ def get_order_by_shk(api_key, shk_id):
 
         data = response.json()
         orders = data.get("orders", [])
-
         if not orders:
-            break  # Если заказов нет, завершаем цикл
+            break 
 
         for order in orders:
             skus = order.get("skus", [])
 
-            # Приводим все к строкам для корректного сравнения
             skus_str = [str(sku) for sku in skus]
 
             if str(shk_id) in skus_str:
-                return order, None  # Если нашли — возвращаем заказ
+                return order, None  
 
-        next_page = data.get("next", 0)  # Получаем `next`
+        next_page = data.get("next", 0) 
         if next_page == 0:
-            break  # Если `next_page` = 0, значит, страниц больше нет
+            break  
 
     return None, "Заказ с таким штрихкодом не найден"
 
@@ -59,7 +57,6 @@ def get_feedback_by_shk(api_key, shk_id):
     Получает единственный отзыв по штрих-коду товара (ShkId).
     Добавляет проверку, был ли уже выплачен кэшбэк за этот отзыв.
     """
-    # Проверка на уже выплаченный кэшбэк
     if check_cashback_payment(shk_id):
         return None, "Кэшбэк за этот отзыв уже был выплачен ранее"
 
@@ -69,9 +66,9 @@ def get_feedback_by_shk(api_key, shk_id):
     except ValueError:
         return None, "❌ Некорректный формат штрихкода!"
 
-    for answered in [False, True]:  # Проверяем как отвеченные, так и неотвеченные отзывы
+    for answered in [False, True]: 
         params = {
-            "take": 5000,  # Берем максимум отзывов
+            "take": 5000, 
             "skip": 0,
             "isAnswered": str(answered).lower(),
             "order": "dateDesc"
@@ -83,14 +80,12 @@ def get_feedback_by_shk(api_key, shk_id):
 
         feedbacks = response.json().get("data", {}).get("feedbacks", [])
 
-        # Ищем отзыв с нужным `shkId`
         for feedback in feedbacks:
             if int(feedback.get('lastOrderShkId', 0)) == shk_id:
-                return feedback, None  # Возвращаем первый найденный отзыв
+                return feedback, None
 
     return None, "❌ Отзыв по этому штрихкоду не найден!"
 
-#Для сценария обработки ошибок
 def get_product_info(api_key, nm_id):
     """
     Получает информацию о товаре по его `nmId`: название, бренд, цена, категория.
@@ -99,14 +94,13 @@ def get_product_info(api_key, nm_id):
 
     payload = {
         "settings": {
-            "cursor": {"limit": 1},  # Запрашиваем только 1 карточку
+            "cursor": {"limit": 1},
             "filter": {
-                "textSearch": str(nm_id)  # Ищем карточку по nmID
+                "textSearch": str(nm_id) 
             }
         }
     }
 
-    # Получаем карточку товара
     response = requests.post(CONTENT_API_URL, json=payload, headers=headers)
 
     if response.status_code != 200:
@@ -118,10 +112,9 @@ def get_product_info(api_key, nm_id):
 
     card_info = cards[0]
 
-    # Получаем цену
     params = {
-        "filterNmID": nm_id,  # Поиск по артикулу WB
-        "limit": 1  # Нам нужен один товар
+        "filterNmID": nm_id, 
+        "limit": 1 
     }
     response = requests.get(PRICES_API_URL, headers=headers, params=params)
 
@@ -129,11 +122,10 @@ def get_product_info(api_key, nm_id):
         return None, f"Ошибка получения цены: {response.status_code}"
 
     data = response.json().get("data", {}).get("listGoods", [])
-    product_price_info = data[0]  # Берем первый найденный товар
-    # Получаем категории товаров
+    product_price_info = data[0] 
     params = {
-        "limit": 1000,  # Максимум товаров, можно уменьшить если нужно
-        "locale": "ru",  # Русский язык
+        "limit": 1000, 
+        "locale": "ru", 
     }
     response = requests.get(OBJECTS_API_URL, headers=headers, params=params)
     if response.status_code != 200:
